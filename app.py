3f64210b9f57caf38c0de75cdbf1aa80f7b70213
saurabh <saurabhmehta1601@gmail.com>
2022-02-04 21:41:10 +0530

    refactor app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import pickle 
-import os 
 from flask import Flask , request
 
 app = Flask(__name__)
 
 @app.route('/' , methods=['GET',"POST"])
-def home():
+def index():
     if request.method == 'GET':
         return "Got a get request"
-    filePath = open(os.path.join("ml-models","iris-model.pkl" ), "rb") 
+    filePath = open("iris.pkl" , "rb") 
     model = pickle.load(filePath)
     sl = request.form['sepal_length'] 
     sw = request.form['sepal_width']
@@ -17,6 +16,3 @@ def home():
     input = [sl, sw , pl , pw ]
     predictions = model.predict([input])
     return f"{predictions[0]}"
-
-if __name__ == '__main__':
-    app.run()
